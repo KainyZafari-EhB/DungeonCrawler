d1a82e64ccfa6ff0cd91e8431c21ab7e00cdd664
Kainy Zafari <160415138+KainyZafari-EhB@users.noreply.github.com>
2026-08-11 00:03:49 +0200

    Player class implemented to store Positions (X, Y) Grid class implemented

---
+++ src/Grid.java
@@ -0,0 +1,52 @@
+public class Grid {
+    int width = 1;
+    int height = 1;
+
+    char[][] cellList;
+
+
+    public Grid(){
+        this(1, 1);
+    }
+
+    public Grid(int width, int height){
+        cellList = new char[width][height];
+        this.width = width;
+        this.height = height;
+    }
+
+    void fillGrid(){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                cellList[i][j] = '.';
+            }
+        }
+    }
+
+    //TODO: prints only one row of .'s;
+    void printGrid(){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                System.out.println(cellList[i][j]);
+            }
+        }
+    }
+
+
+    //getter height
+    public int getHeight() {
+        return height;
+    }
+    //setter height
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    //getter width
+    public int getWidth() {
+        return width;
+    }
+    //setter width
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
