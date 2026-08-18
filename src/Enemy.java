304f8fcc5c589b729471e64b2d2572a2d2ddb082
Kainy Zafari <160415138+KainyZafari-EhB@users.noreply.github.com>
2026-08-18 19:13:11 +0200

    Enemies have a draw function to draw on grid. Enemy constructor with a grid.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,13 +1,32 @@
+import java.util.Random;
+
+//TODO: Implement a generate enemies function to mass spawn enemies, Give enemies health, Make a damage to player function, generate enemies on the grid;
 public class Enemy {
 
     int positionX;
     int positionY;
 
-    public Enemy(){this(0,0);}
+    Grid grid;
+    char[][] cellList = grid.cellList;
 
-    public Enemy(int positionX, int positionY){
+    public Enemy(int positionX, int positionY, Grid grid){
         this.positionX = positionX;
         this.positionY = positionY;
+        this.grid = grid;
+    }
+
+    ///draws an enemy at a random coordinate;
+    public void drawEnemy(){
+        Random randomNumbers = new Random();
+
+        int randomX = randomNumbers.nextInt(0, grid.getWidth());
+        int randomY = randomNumbers.nextInt(0, grid.getHeight());
+        setPositionX(randomX);
+        setPositionY(randomY);
+
+        if (cellList != null){
+            cellList[randomX][randomY] = 'O';
+        }
     }
 
     public int getPositionX() {
@@ -26,6 +45,7 @@ public class Enemy {
         this.positionY = positionY;
     }
 
+    /// set Coordinates of both X and Y for the enemy
     public void setMeasurements(int positionX, int positionY) {
         setPositionX(positionX);
         setPositionY(positionY);
